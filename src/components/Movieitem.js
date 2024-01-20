8a5ed1be91a35118dbe2c2f90de25027734100ad
azure0929 <chary8028@naver.com>
2024-01-20 23:24:24 +0900

    Docs : 대체 이미지 변경

--- src/components/Movieitem.js
+++ src/components/Movieitem.js
@@ -1,6 +1,5 @@
 import { Component } from '../core/assets';
 
-// MovieItem 클래스명으로 하여 tagName을 a로 하여 클릭 시 Movie page로 이동하도록 구성
 export default class MovieItem extends Component {
   constructor(props) {
     super({
@@ -11,7 +10,6 @@ export default class MovieItem extends Component {
   render() {
     const { movie } = this.props;
 
-    // setArrtribute 기능으로 #/movie 해당 imdbID로 출력되도록 구성
     this.el.setAttribute('href', `#/movie?id=${movie.imdbID}`);
     this.el.classList.add('movie');
     this.el.innerHTML = /* html */ `
@@ -26,8 +24,7 @@ export default class MovieItem extends Component {
           ${movie.Type}
         </div>
       </div>
-      <!-- 영화 포스터가 없을 경우 onerror 속성을 통해 대체 이미지 '스즈메의 문단속 포스터' 출력 -->
-      <img src="${movie.Poster}"  onerror="this.src='https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86815/86815_1000.jpg'" />
+      <img src="${movie.Poster}"  onerror="this.src='https://fl-1.cdn.flockler.com/embed/no-image.svg'"alt="noimage" />
     `;
   }
 }
