4dbd627c003395e061b01536f023af3b176fded8
azure0929 <chary8028@naver.com>
2024-01-29 19:47:15 +0900

    docs: totop 삭제

--- src/script.js
+++ src/script.js
@@ -1,16 +1,7 @@
-import App from './App';
-import router from './routes';
+import App from "./App";
+import router from "./routes";
 
-const info = document.querySelector('.root');
+const info = document.querySelector(".root");
 info.append(new App().el);
 
 router();
-
-const toTopEl = document.querySelector('.to-top');
-
-toTopEl.onclick = () => {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  });
-};
